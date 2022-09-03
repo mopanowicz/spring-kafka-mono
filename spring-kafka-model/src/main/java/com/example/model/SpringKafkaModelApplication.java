86d4e6678a5701c3fd2b3591151df24dd9a17714
Marek Opanowicz <marek.opanowicz@gmail.com>
2022-09-03 21:53:40 +0200

    Initial modules

---
+++ spring-kafka-model/src/main/java/com/example/model/SpringKafkaModelApplication.java
@@ -0,0 +1,13 @@
+package com.example.model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaModelApplication.class, args);
+	}
+
+}
