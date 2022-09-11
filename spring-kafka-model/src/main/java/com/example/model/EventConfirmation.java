319a08430d9cab96014da52e7f2bbc8396e075fd
Marek Opanowicz <marek.opanowicz@gmail.com>
2022-09-11 09:52:29 +0200

    Mark uuids as indexed

--- spring-kafka-model/src/main/java/com/example/model/EventConfirmation.java
+++ spring-kafka-model/src/main/java/com/example/model/EventConfirmation.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.index.Indexed;
 
 import java.time.LocalDateTime;
 
@@ -12,6 +13,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class EventConfirmation {
+    @Indexed
     String eventUuid;
     LocalDateTime eventReceived;
 }
---
+++ spring-kafka-monitor/src/main/java/com/example/monitor/service/EventMonitor.java
@@ -0,0 +1,2 @@
+package com.example.monitor.service;public class EventMonitor {
+}
