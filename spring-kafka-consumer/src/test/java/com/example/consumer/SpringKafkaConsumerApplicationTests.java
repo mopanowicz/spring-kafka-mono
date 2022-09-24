d587ea80daf21c59bb9d5aa9f5221e039316f7dc
Marek Opanowicz <marek.opanowicz@gmail.com>
2022-09-24 19:09:19 +0200

    Add kafka test configuration

--- spring-kafka-consumer/src/test/java/com/example/consumer/SpringKafkaConsumerApplicationTests.java
+++ spring-kafka-consumer/src/test/java/com/example/consumer/SpringKafkaConsumerApplicationTests.java
@@ -2,9 +2,11 @@ package com.example.consumer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 
 @SpringBootTest
-class SpringKafkaConsumerApplicationTests {
+@EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:19092", "port=19092"})
+public class SpringKafkaConsumerApplicationTests {
 
 	@Test
 	void contextLoads() {
