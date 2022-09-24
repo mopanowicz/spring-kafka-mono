5b6e034e244a82c100bb10d1fcb663a68ddcfe4b
Marek Opanowicz <marek.opanowicz@gmail.com>
2022-09-24 19:20:12 +0200

    Add kafka test configuration for monitor project

--- spring-kafka-consumer/src/test/java/com/example/consumer/SpringKafkaConsumerApplicationTests.java
+++ spring-kafka-consumer/src/test/java/com/example/consumer/SpringKafkaConsumerApplicationTests.java
@@ -3,8 +3,10 @@ package com.example.consumer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:19092", "port=19092"})
 public class SpringKafkaConsumerApplicationTests {
 
--- spring-kafka-monitor/src/test/java/com/example/monitor/SpringKafkaMonitorApplicationTests.java
+++ spring-kafka-monitor/src/test/java/com/example/monitor/SpringKafkaMonitorApplicationTests.java
@@ -2,8 +2,12 @@ package com.example.monitor;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:19092", "port=19092"})
 class SpringKafkaMonitorApplicationTests {
 
 	@Test
