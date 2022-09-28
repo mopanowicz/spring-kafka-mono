f2e53f54de6152c2198d5787481ba868b65a2605
Marek Opanowicz <marek.opanowicz@gmail.com>
2022-09-28 21:49:39 +0200

    Rename event document repository

--- spring-kafka-consumer/src/main/java/com/example/consumer/event/EventDocumentRepository.java
+++ spring-kafka-consumer/src/main/java/com/example/consumer/event/EventDocumentRepository.java
@@ -3,5 +3,5 @@ package com.example.consumer.event;
 import com.example.model.document.EventDocument;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface EventRepository extends MongoRepository<EventDocument, String> {
+public interface EventDocumentRepository extends MongoRepository<EventDocument, String> {
 }
--- spring-kafka-consumer/src/main/java/com/example/consumer/event/EventService.java
+++ spring-kafka-consumer/src/main/java/com/example/consumer/event/EventService.java
@@ -7,21 +7,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class EventService {
 
-    private final EventRepository eventRepository;
+    private final EventDocumentRepository eventDocumentRepository;
 
     @Transactional("transactionManager")
     Event saveEvent(Event event) {
         log.debug("saveEvent event={}", event);
         event.setSaved(System.currentTimeMillis());
-        eventRepository.save(new EventDocument(event));
+        eventDocumentRepository.save(new EventDocument(event));
         return event;
     }
 }
